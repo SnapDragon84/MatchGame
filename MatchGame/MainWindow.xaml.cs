7329dbd43be2a63b04e10dc9d5d5b3876959d7ca
James R. Smith <jamessmith84@comcast.net>
2022-11-05 15:10:25 -0600

    Finished coding MatchGame for timer et al

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -15,19 +15,41 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
+    //To add a timer to our game code, we must 1st add the following import from another namespace
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();  //initializes a new instance of the DispatcherTimer class
+        int tenthsOfSecondsElapsed; //to keep track of the elapsed time
+        int matchesFound;   //to keep track of the number of matches the player has found
+
         public MainWindow() //constructor
         {
+            timer.Interval = TimeSpan.FromSeconds(.1);  //tell the timer how frequently to "tick"
+            timer.Tick += Timer_Tick;   //as soon as we typed += the IDE displayed "Press TAB to insert" & added "Timer_Tick;" & the method below
+
             InitializeComponent();
             SetUpGame();    //call to method()
         }
 
-        private void SetUpGame()
+        //The Timer_Tick method will update a TextBlock that spans the entire bottom row of the grid.
+        private void Timer_Tick(object sender, EventArgs e)
         {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s"); //replaces text "Elapsed time"
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
+            //throw new NotImplementedException();
+        }
+
+        private void SetUpGame()
+        {            
             //throw new NotImplementedException();
             List<string> animalEmoji = new List<string>()   //Windows LoGo button on keyboard + period (.)
             {
@@ -45,10 +67,52 @@ namespace MatchGame
 
             foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())   //iterates over/trhough each TextBlock in mainGrid
             {
-                int index = random.Next(animalEmoji.Count); //index is assigned random.Next (...Count) gets the number of elements from in/left in List <T>
-                string nextEmoji = animalEmoji[index];  //nextEmoji is assigned animalEmoji at index selected from above
-                textBlock.Text = nextEmoji; //nextEmoji is assigned to this particular TextBlock in the mainGrid panel
-                animalEmoji.RemoveAt(index);    //as each animalEmoji is chosen by index, it is then removed from List<T> to avoid duplication
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count); //index is assigned random.Next (...Count) gets the number of elements from in/left in List <T>
+                    string nextEmoji = animalEmoji[index];  //nextEmoji is assigned animalEmoji at index selected from above
+                    textBlock.Text = nextEmoji; //nextEmoji is assigned to this particular TextBlock in the mainGrid panel
+                    animalEmoji.RemoveAt(index);    //as each animalEmoji is chosen by index, it is then removed from List<T> to avoid duplication
+                }
+            }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;   //initially set to 0 here
+        }
+
+        //fields
+        private TextBlock lastTextBlockClicked; //VS prompted for "accessibility modifier required" -> chose private
+        private bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e) //auto created when double-click in MouseDown property in Event Handler
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e) //checks the matchesFound field
+        {
+            if(matchesFound == 8)
+            {
+                SetUpGame();    //resets the game if all 8 pairs have been found; otherwise it will do nothing because the game is still running
             }
         }
     }
